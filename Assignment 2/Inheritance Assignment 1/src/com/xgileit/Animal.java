6a364f077088502bea2743ffd27059f319e55ea5
Mpho Makhari <110976611+MphoMakhari@users.noreply.github.com>
2022-09-25 19:17:23 +0200

    Add files via upload

---
+++ Assignment 2/Inheritance Assignment 1/src/com/xgileit/Animal.java
@@ -0,0 +1,53 @@
+package com.xgileit;
+
+/**
+ * @author Mpho Makhari
+ * 24/09/2022
+ * Java Intern 2022
+ */
+
+
+public class Animal {
+
+
+
+    private String typeOfAnimal;
+
+    /**
+     * Default constructor
+     * I have set Mammal as an initial value since Dog is a given example
+     *
+     */
+    public Animal() {
+     this("Mammal");
+    }
+
+    /**+
+     * Overloaded constructor to change value typeofAnimal
+     * @param typeOfAnimal new value
+     */
+    public Animal(String typeOfAnimal) {
+        setTypeOfAnimal(typeOfAnimal);
+    }
+
+    /**
+     * Change the type of animal object
+     * @param typeOfAnimal The new value that will be sent to the overloaded constructor
+     */
+    public void setTypeOfAnimal(String typeOfAnimal) {
+        this.typeOfAnimal = typeOfAnimal;
+    }
+
+    /**
+     * Returns the new value
+     * @return The new Type of Animal
+     */
+    public String getTypeOfAnimal() {
+        return typeOfAnimal;
+    }
+
+    @Override
+    public String toString() {
+        return "Type Of Animal: " + typeOfAnimal ;
+    }
+}
---
+++ Assignment 2/Inheritance Assignment 1/src/com/xgileit/DogApplication.java
@@ -0,0 +1,22 @@
+package com.xgileit;
+
+/**
+ * Dog Application
+ * @author Mpho Makhari
+ * 24/09/2022
+ */
+public class DogApplication {
+
+    public static void main(String[] args)
+    {
+        Dog objDog = new Dog();
+
+        System.out.println("***************Initial values*****************"  );
+        System.out.println(objDog.toString()); //Display default values
+
+        System.out.println("***************New values*****************"  );
+        objDog = new Dog("Mammal","Dog","German Shepard"); //Instatiate the object
+        System.out.println(objDog.toString());  //Display new  values
+    }
+
+}
