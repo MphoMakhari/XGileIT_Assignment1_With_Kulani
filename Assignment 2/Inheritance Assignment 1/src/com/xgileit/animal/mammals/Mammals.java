1af9ed70b0d579939ee0859e156f0f5dea88da6a
Mpho Makhari <110976611+MphoMakhari@users.noreply.github.com>
2022-09-26 07:39:26 +0200

    Add files via upload

---
+++ Assignment 2/Inheritance Assignment 1/src/com/xgileit/animal/mammals/Mammals.java
@@ -0,0 +1,54 @@
+package com.xgileit.animal.mammals;
+
+import com.xgileit.animal.Animal;
+
+/**
+ * @author Mpho Makhari
+ * Date: 24/09/2022
+ */
+public class Mammals extends Animal {
+
+    private String typeOfMammal;
+
+    /**
+     * Default constructor
+     */
+    public Mammals() {
+        this("Mammal","Dog");
+    }
+
+    /**
+     * Overloaded constructor for Mammal Class
+     * @param typeOfAnimal new value
+     * @param typeOfMammal new value
+     */
+    public Mammals(String typeOfAnimal, String typeOfMammal) {
+        super(typeOfAnimal);
+        setTypeOfMammal(typeOfMammal);
+    }
+
+    /**
+     * Access typeOfMammal Object
+     * @return the type of mammal
+     */
+    public String getTypeOfMammal() {
+        return typeOfMammal;
+    }
+
+    /**
+     * Change typeOfMammal object
+     * @param typeOfMammal new value
+     */
+    public void setTypeOfMammal(String typeOfMammal) {
+        this.typeOfMammal = typeOfMammal;
+    }
+
+    /**
+     *This methods will print the class values
+     * @return Animal object and Mammals object
+     */
+    @Override
+    public String toString() {
+        return super.toString() + "\nType Of Mammal: " + typeOfMammal ;
+    }
+}
---
+++ Assignment 2/Inheritance Assignment 1/src/com/xgileit/animal/mammals/dog/Dog.java
@@ -0,0 +1,53 @@
+package com.xgileit.animal.mammals.dog;
+
+import com.xgileit.animal.mammals.Mammals;
+
+/**
+ * @author Mpho Makhari
+ * XGileIT Intern 2022
+ * Sep 2022
+ */
+public class Dog extends Mammals {
+
+    //Instance variables
+    private String breed;
+
+    /**
+     * Default Constructor
+     */
+    public Dog() {
+        this("Mammal", "Dog", "Bull Dog");
+    }
+
+    /**
+     * Overloaded Constructor
+     * @param typeOfAnimal new value
+     * @param typeOfMammal new value
+     * @param breed new value
+     */
+    public Dog(String typeOfAnimal, String typeOfMammal, String breed) {
+        super(typeOfAnimal, typeOfMammal);
+        setBreed(breed);
+    }
+
+    /**
+     * Change the value of the breed
+     * @param breed the new value
+     */
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    /**
+     * Return the new value
+     * @return breed value
+     */
+    public String getBreed() {
+        return breed;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nType of breed: " + breed ;
+    }
+}
